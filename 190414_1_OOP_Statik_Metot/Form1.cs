c9c16946d5bf4d9f4e8d5b6e328b9b75617d2b4c
Hasan BİLGİN <hasanbilgintr@hotmail.com>
2019-04-14 09:52:51 +0300

    Add project files.

---
+++ 190414_1_OOP_Statik_Metot/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _190414_1_OOP_Statik_Metot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
